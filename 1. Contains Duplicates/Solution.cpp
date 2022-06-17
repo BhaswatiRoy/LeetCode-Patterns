a8f69ad8ba8c58ae4f954158edd495f612779f48
Bhaswati Roy <roybhaswati24@gmail.com>
2022-06-17 11:00:37 +0530

    Create Solution.cpp

---
+++ 1. Contains Duplicates/Solution.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+  //brute - running 2 nested loops to check - O(n^2)
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=0;j<nums.size();j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    //better - sorting and comparing adjacents - O(nlogn)
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]==nums[i-1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    //Optimal - using maps to store frequency - O(n)
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        for(auto it=mp.begin();it!=mp.end();it++)
+        {
+            if(it->second>=2)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
