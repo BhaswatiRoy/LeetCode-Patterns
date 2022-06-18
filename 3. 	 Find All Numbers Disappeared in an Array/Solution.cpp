9ea4e46bcd3848eb4ead6309f1768c738edd07c7
Bhaswati Roy <roybhaswati24@gmail.com>
2022-06-18 21:30:13 +0530

    Create Solution.cpp

---
+++ 3. 	 Find All Numbers Disappeared in an Array/Solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) 
+    {
+        unordered_map<int,int>mp;
+        //store the elements between 1 to n which are in the vector 
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        vector<int>ans;
+        int n=nums.size();
+        //looping from 1 to n
+        for(int j=1;j<=n;j++)
+        {
+            //if the value j is not in the map 
+            if(mp.find(j)==mp.end())
+            {
+                //then push it in the ans
+                ans.push_back(j);
+            }
+        }
+        return ans;
+    }
+};
